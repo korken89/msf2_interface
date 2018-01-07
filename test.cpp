3df753c033970d596874937791132f2814e54ce4
Emil Fresk <emil.fresk@gmail.com>
2018-01-07 21:55:14 +0100

    Added more for testing implementation and error checking

--- test.cpp
+++ test.cpp
@@ -3,7 +3,24 @@
 #include <initializer_list>
 #include <tuple>
 
+#include <kvasir/mpl/mpl.hpp>
+
 // Helper functions
+using namespace kvasir;
+
+template <typename... Ts>
+using distinct = mpl::bool_<
+                  mpl::call<
+                    mpl::sort<
+                      mpl::is_same<>,
+                      mpl::remove_adjacent<
+                        mpl::is_same<>,
+                        mpl::size<>
+                      >
+                    >,
+                    Ts...
+                  >::value == mpl::call< mpl::size<>, Ts...>::value >;
+
 template<std::size_t... S>
 constexpr std::size_t sum()
 {
@@ -52,41 +69,55 @@ enum class error
 struct sensor_base_tag {};
 
 // A sensor needs to include ...
-template <std::size_t MEASUREMENT_SIZE,
-          std::size_t NUM_LINEAR_STATES,
-          std::size_t NUM_ROTATION_STATES>
+template <std::size_t MeasurementSize,
+          std::size_t NumLinearStates = 0,
+          std::size_t NumRotationStates = 0>
 struct sensor_base : sensor_base_tag
 {
   using measurement_vector_size =
-      std::integral_constant< std::size_t, MEASUREMENT_SIZE >;
+      std::integral_constant< std::size_t, MeasurementSize >;
   using num_linear_states =
-      std::integral_constant< std::size_t, NUM_LINEAR_STATES >;
+      std::integral_constant< std::size_t, NumLinearStates >;
   using num_rotation_states =
-      std::integral_constant< std::size_t, NUM_ROTATION_STATES >;
+      std::integral_constant< std::size_t, NumRotationStates >;
 };
 
 // msf2 specs, takes a list of unique sensors
 template <typename... Sensors>
 struct msf2_specs
 {
+  // Check that input list is contains only unique sensors
+  static_assert(sizeof...(Sensors) > 0,
+                "There are no sensors defined and there must be at least one.");
+
   // Check that input list is actually sensors
-  static_assert(all_true< std::is_base_of<sensor_base_tag, Sensors>::value ... >(),
-                "Not all elements in the sensor list is a sensor, did you forget to "
-                "inherit from sensor_base?");
+  static_assert(
+      all_true< std::is_base_of< sensor_base_tag, Sensors >::value... >(),
+      "Not all elements in the sensor list is a sensor, did you forget to "
+      "inherit from sensor_base?");
+
+  // Check that input list is contains only unique sensors
+  static_assert(
+      distinct< Sensors... >::value,
+      "The list does not only contain unique sensors, remove duplicates.");
 
   // Define state sizes
-  using num_core_states         = std::integral_constant< std::size_t, 16 >;
-  using num_core_error_states   = std::integral_constant< std::size_t, 15 >;
-  using num_sensor_states       = std::integral_constant<
-                                    std::size_t,
-                                    sum<Sensors::num_linear_states::value...>() +     // Get num linear state
-                                    4 * sum<Sensors::num_rotation_states::value...>() // Get num rotational states
-                                  >;
+  using num_core_states       = std::integral_constant< std::size_t, 16 >;
+  using num_core_error_states = std::integral_constant< std::size_t, 15 >;
+  using num_sensor_states     = std::integral_constant<
+      std::size_t,
+      sum< Sensors::num_linear_states::value... >() +  // Get num linear state
+          4 * sum< Sensors::num_rotation_states::value... >()  // Get num
+                                                               // rotational
+                                                               // states
+      >;
   using num_sensor_error_states = std::integral_constant<
-                                    std::size_t,
-                                    sum<Sensors::num_linear_states::value...>() +     // Get num linear state
-                                    3 * sum<Sensors::num_rotation_states::value...>() // Get num rotational states
-                                  >;
+      std::size_t,
+      sum< Sensors::num_linear_states::value... >() +  // Get num linear state
+          3 * sum< Sensors::num_rotation_states::value... >()  // Get num
+                                                               // rotational
+                                                               // states
+      >;
 
   // Storage for sensors with state
   using sensor_storage_t = std::tuple< Sensors... >;
